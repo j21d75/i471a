c72af1399ae20be1f885a3c360c8c463bd5abc8f
Jared Davis <jdavis75@cs.binghamton.edu>
2023-09-12 17:46:15 -0400

    finished?

--- submit/prj1-sol/ast.py
+++ submit/prj1-sol/ast.py
@@ -58,56 +58,99 @@ def parse(text):
             return tok
 
     def program():
-        jsons = []
+        asts = []
         while (not peek('EOF')):
-            jsons.append(declaration())
-        return jsons
+            asts.append(declaration())
+        return asts
 
     def declaration():
-        r = reserved()        
+        r = reserved()
         s = struct()
         consume('=')
         i = iden()
         consume(';')
+        return ast(r, i, s)
        
     def reserved():
-        if (peek('const')):
-            consume('const')
-        elif (peek('let')):
-            consume('let')
+        if (peek('const') or (peek('let'))):
+            kind = lookahead.kind
+            consume(kind)
+            return kind
+        else:
+            print(f'missing declaration', file=sys.stderr)
+            sys.exit(1)
 
     def struct():
         if (peek('[')):
             a = array()
-        elif (peek('{'):
+            return a
+        elif (peek('{')):
             o = obj()
+            return o
         else:
             i = iden()
+            return i
 
     def array():
+        arr = []
         consume('[')
-        s = struct()
+        if (peek(']')):
+            print(f'empty array', file=sys.stderr)
+            sys.exit(1)
+        s = (struct())
+        arr.append(s)
         while(peek(',')):
             consume(',')
-            s1 = struct()
+            if (peek(']')):
+                print(f'extra comma', file=sys.stderr)
+                sys.exit(1)
+            s1 = (struct())
+            arr.append(s1)
+        if (peek('ALNU')):
+            print(f'missing comma', file=sys.stderr)
+            sys.exit(1)
         consume(']')
+        return arr
 
     def obj():
+        Dict = {}
+        ind = 0
         consume('{')
-        k = key()
+        if (peek('}')):
+            print(f'empty object-struct', file=sys.stderr)
+            sys.exit(1)
+        k1, k2 = key()
+        Dict[k1] = k2
         while(peek(',')):
             consume(',')
-            k1 = key()
+            if (peek('}')):
+                print(f'extra comma', file=sys.stderr)
+                sys.exit(1)
+            k3, k4 = key()
+            Dict[k3] = k4
         consume('}')
+        return Dict
 
     def key():
         i = iden()
         if (peek(':')):
             consume(':')
             s = struct()
+            return i, s
+        elif (peek('ALNU')):
+            print(f'missing colon', file=sys.stderr)
+            sys.exit(1)
+        else:
+            return i, i
 
     def iden():
         if (peek('ALNU')):
+            value = lookahead.lexeme
+            consume('ALNU')
+            return value
+        elif (peek('const') | peek('let')):
+            print(f'reserved id', file=sys.stderr)
+            sys.exit(1)
 
     #begin parse()
     tokens = scan(text)
@@ -120,12 +163,18 @@ def parse(text):
     return value
 
 def scan(text):
-    SPACE_RE = re.compile(r'\s+')
+    SPACE_RE = re.compile(r'\s+|//.*')
+    CONST_RE = re.compile(r'const')
+    LET_RE = re.compile(r'let')
     ALNU_RE = re.compile(r'[a-zA-Z_]\w*')
     CHAR_RE = re.compile(r'.')
     def next_match(text):
         m = SPACE_RE.match(text)
         if (m): return (m, None)
+        m = CONST_RE.match(text)
+        if (m): return (m, 'const')
+        m = LET_RE.match(text)
+        if (m): return (m, 'let')
         m = ALNU_RE.match(text)
         if (m): return (m, 'ALNU')
         m = CHAR_RE.match(text)  #must be last: match any char
@@ -140,24 +189,13 @@ def scan(text):
     return tokens
 
 def main():
-    if (len(sys.argv) != 2): usage();
-    contents = readFile(sys.argv[1]);
-    jsons = parse(contents)
-    print(json.dumps(jsons, separators=(',', ':'))) #no whitespace
-
-def readFile(path):
-    with open(path, 'r') as file:
-        content = file.read()
-    return content
-
-
-def usage():
-    print(f'usage: {sys.argv[0]} DATA_FILE')
-    sys.exit(1)
+    contents = sys.stdin.read();
+    asts = parse(contents)
+    print(json.dumps(asts, separators=(',', ':'))) #no whitespace
 
 #use a dict so that we can add attributes dynamically
-def json(decl-spec, gId, *gStruct):
-    return { 'decl': decl-spec, 'id': gId, 'struct': gStruct }
+def ast(declSpec, gId, gStruct):
+    return { 'decl': declSpec, 'id': gId, 'struct': gStruct }
 
 Token = namedtuple('Token', ['kind', 'lexeme'])
 
--- submit/prj1-sol/decls.sh
+++ submit/prj1-sol/decls.sh
@@ -2,7 +2,7 @@
 
 #sets dir to directory containing this script
 dir=`dirname $0`
-
+./ast.py
 #use $dir/ as prefix to run any programs in this dir
 #so that this script can be run from any directory.
 
